1e65eea6e3edf0e010ff3a089e6412d5c0588a5b
Bohirjon Akhmedov <bohir@silkroadprofessionals.com>
2021-05-01 11:43:02 +0500

    init project

---
+++ Glassmorphosim/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Glassmorphosim
+//
+//  Created by Bohirjon Akhmedov on 01/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Glassmorphosim/GlassmorphosimApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassmorphosimApp.swift
+//  Glassmorphosim
+//
+//  Created by Bohirjon Akhmedov on 01/05/21.
+//
+
+import SwiftUI
+
+@main
+struct GlassmorphosimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
